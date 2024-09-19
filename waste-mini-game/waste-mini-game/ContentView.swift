279dc579fa699c77b9fd7f9abec50639252fa2d1
Justin Chung <jdchung@scu.edu>
2024-09-18 19:14:31 -0700

    Initial commit

---
+++ waste-mini-game/waste-mini-game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  waste-mini-game
+//
+//  Created by Justin Chung on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ waste-mini-game/waste-mini-game/waste_mini_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  waste_mini_gameApp.swift
+//  waste-mini-game
+//
+//  Created by Justin Chung on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct waste_mini_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
