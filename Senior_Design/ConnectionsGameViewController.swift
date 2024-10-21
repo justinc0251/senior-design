19bedf620df7aff6b630d4aee76d4e6663023417
aster <asli@scu.edu>
2024-10-20 20:36:29 -0700

    Implement 2min timer for game, implement the game to continue until all of the groups are found

--- Senior_Design/ConnectionsGameViewController.swift
+++ Senior_Design/ConnectionsGameViewController.swift
@@ -3,10 +3,11 @@ import UIKit
 class ConnectionsGameViewController: UIViewController {
     
     let gridSize = 4
-    
     var selectedButtons: Set<UIButton> = []
-    
     var attemptsLeft = 4
+    var gameTimer: Timer?
+    var remainingTime = 120 //2 minutes
+    var correctGroups: Set<Set<Int>> = []
     
     let tileGroups: [[Int]] = [
         [0,1,2,3], //Landfill
@@ -23,12 +24,22 @@ class ConnectionsGameViewController: UIViewController {
         label.textAlignment = .center
         return label
     }()
+    
+    private let timerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Time Left: 2:00"
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.textAlignment = .center
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupGrid()
         setupAttemptsLabel()
+        startTimer()
     }
 
     private func setupGrid() {
@@ -51,9 +62,14 @@ class ConnectionsGameViewController: UIViewController {
     
     private func setupAttemptsLabel() {
         view.addSubview(attemptsLabel)
+        view.addSubview(timerLabel)
+        
         NSLayoutConstraint.activate([
             attemptsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            attemptsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            attemptsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            timerLabel.topAnchor.constraint(equalTo: attemptsLabel.bottomAnchor, constant: 10),
+            timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
 
@@ -81,26 +97,27 @@ class ConnectionsGameViewController: UIViewController {
     }
 
     private func checkForConnection() {
-        let selectedTags = selectedButtons.map { $0.tag }
+        let selectedTags = Set(selectedButtons.map { $0.tag })
         
         if isValidGroup(selectedTags) {
-            showWinAlert()
+            correctGroups.insert(selectedTags)
+            resetSelection()
+            
+            if correctGroups.count == tileGroups.count {
+                showWinAlert()
+            }
         } else {
             decrementAttempts()
             resetSelection()
         }
     }
     
-    private func isValidGroup(_ tags: [Int]) -> Bool {
-        for group in tileGroups {
-            if Set(group).isSuperset(of: Set(tags)) {
-                return true
-            }
-        }
-        return false
+    private func isValidGroup(_ tags: Set<Int>) -> Bool {
+        return tileGroups.contains{ Set($0) == tags }
     }
 
     private func showWinAlert() {
+        gameTimer?.invalidate() //stop timer
         let alert = UIAlertController(
             title: "You Win!",
             message: "You connected 4 tiles!",
@@ -122,14 +139,32 @@ class ConnectionsGameViewController: UIViewController {
         attemptsLabel.text = "Attempts Left: \(attemptsLeft)"
         
         if attemptsLeft == 0 {
-            showGameOverAlert()
+            showGameOverAlert(reason: "You've used all your attempts!")
         }
     }
     
-    private func showGameOverAlert() {
+    private func startTimer() {
+        gameTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateTimer() {
+        if remainingTime > 0 {
+            remainingTime -= 1
+            let minutes = remainingTime / 60
+            let seconds = remainingTime % 60
+            timerLabel.text = String(format: "Time Left: %01d:%02d", minutes, seconds)
+        } else {
+            gameTimer?.invalidate()
+            let message = attemptsLeft > 0 ? "The timer ran out!" : "You've used all your attempts!"
+            showGameOverAlert(reason: message)
+        }
+    }
+    
+    private func showGameOverAlert(reason: String) {
+        gameTimer?.invalidate() //stop timer
         let alert = UIAlertController(
             title: "Game Over",
-            message: "You've used all your attempts!",
+            message: reason,
             preferredStyle: .alert
         )
         alert.addAction(UIAlertAction(title: "OK", style: .default) {_ in self.resetGame()
@@ -141,6 +176,10 @@ class ConnectionsGameViewController: UIViewController {
         resetSelection()
         attemptsLeft = 4
         attemptsLabel.text = "Attempts Left: 4"
+        remainingTime = 120
+        timerLabel.text = "Time Left: 2:00"
+        correctGroups.removeAll()
+        startTimer()
     }
 }
 
