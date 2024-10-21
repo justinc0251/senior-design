922542937592060d5c2e3a35a113b8df9bb46c1f
Justin Chung <jdchung@scu.edu>
2024-10-20 18:00:01 -0700

    Initial Commit

---
+++ Senior_Design/ConnectionsGameViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+
+class ConnectionsGameViewController: UIViewController {
+    
+    let gridSize = 4
+    
+    var selectedButtons: Set<UIButton> = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupGrid()
+    }
+
+    private func setupGrid() {
+        let buttonSize = view.frame.width / CGFloat(gridSize) - 10
+
+        for row in 0..<gridSize {
+            for col in 0..<gridSize {
+                let button = createButton()
+                button.frame = CGRect(
+                    x: CGFloat(col) * (buttonSize + 10),
+                    y: CGFloat(row) * (buttonSize + 10) + 100,
+                    width: buttonSize,
+                    height: buttonSize
+                )
+                button.tag = row * gridSize + col
+                view.addSubview(button)
+            }
+        }
+    }
+
+    private func createButton() -> UIButton {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(tileTapped(_:)), for: .touchUpInside)
+        return button
+    }
+
+    @objc private func tileTapped(_ sender: UIButton) {
+        if selectedButtons.contains(sender) {
+            sender.backgroundColor = .lightGray
+            selectedButtons.remove(sender)
+        } else {
+            sender.backgroundColor = .blue
+            selectedButtons.insert(sender)
+        }
+        checkForConnection()
+    }
+
+    private func checkForConnection() {
+        if selectedButtons.count == 4 {
+            showWinAlert()
+        }
+    }
+
+    private func showWinAlert() {
+        let alert = UIAlertController(
+            title: "You Win!",
+            message: "You connected 4 tiles!",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
+
+#Preview {
+    ConnectionsGameViewController()
+}
